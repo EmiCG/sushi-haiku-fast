b6eea39818ca6d68d004142c1610cb3a25386661
EmiCG <emicontreras250@gmail.com>
2024-11-22 23:18:03 -0600

    Correccion en enlaces de la api

--- script.js
+++ script.js
@@ -103,10 +103,10 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }
 
-    const sheetId = '1vTlMeaeFiqCvLyrkLJgCBHw-LTF2mTq9mYaJmBAYKMt2sXGPseaKgwqiidEtqvuRKbGkXFi9Nqv3xA9';
+    const sheetId = '16IPSBvan4QeserKoVITGaa69PlB1kEruJIBhSI7eIY0';
     const apiKey = 'AIzaSyDsPbURzpo3Te1e_QzZrw5n-YY8kNjg6Bw';
 
-    fetch(`https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/values/NombreDeHoja?key=${apiKey}`)
+    fetch(`https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/values/Promociones_Haiku_Fast_Sushi?key=${apiKey}`)
         .then(response => response.json())
         .then(data => {
             const rows = data.values;
