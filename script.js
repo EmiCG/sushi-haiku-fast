245cb33ae758cfbae30158575c3609c2dd3dbd05
EmiCG <emicontreras250@gmail.com>
2025-06-13 20:04:36 -0600

    detallitos menores

--- script.js
+++ script.js
@@ -27,61 +27,6 @@ elementosFadeIn.forEach(elemento => {
     observer.observe(elemento);
 });
 
-// document.addEventListener("DOMContentLoaded", function () {
-//     function cargarPromociones(containerId, promociones) {
-//         const promocionesContainer = document.getElementById(containerId);
-//         promocionesContainer.innerHTML = ""; // Limpiar el contenido existente
-
-//         promociones.forEach(promocion => {
-//             const promoDiv = document.createElement("div");
-//             promoDiv.classList.add("promocion");
-
-//             // Aquí construimos correctamente la URL de la imagen
-//             const rutaImagen = `src/promociones/${promocion.imagen}`;
-
-//             promoDiv.innerHTML = `
-//                 <a href="entregas.html">
-//                     <img src="${rutaImagen}" alt="${promocion.titulo}" />
-//                     <h3>${promocion.titulo}</h3>
-//                     <p>${promocion.descripcion}</p>
-//                 </a>
-//             `;
-//             promocionesContainer.appendChild(promoDiv);
-//         });
-//     }
-
-//     const sheetId = '16IPSBvan4QeserKoVITGaa69PlB1kEruJIBhSI7eIY0';
-//     const apiKey = 'AIzaSyDsPbURzpo3Te1e_QzZrw5n-YY8kNjg6Bw';
-
-//     fetch(`https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/values/tablaPromociones?key=${apiKey}`)
-//         .then(response => response.json())
-//         .then(data => {
-//             // Inspeccionamos los datos para verificar que todo esté correcto
-//             console.log(data);
-
-//             const rows = data.values;
-//             const promocionesFijas = [];
-//             const promocionesTemporales = [];
-
-//             // Saltar la primera fila (encabezados) y procesar los datos
-//             for (let i = 1; i < rows.length; i++) {
-//                 const [titulo, descripcion, imagen, tipo] = rows[i];
-//                 const promocion = { titulo, descripcion, imagen };
-
-//                 if (tipo === "fija") {
-//                     promocionesFijas.push(promocion);
-//                 } else if (tipo === "temporal") {
-//                     promocionesTemporales.push(promocion);
-//                 }
-//             }
-
-//             // Cargar las promociones en los contenedores respectivos
-//             cargarPromociones("containerBodyPromocionesFijas", promocionesFijas);
-//             cargarPromociones("containerBodyPromocionesTemporales", promocionesTemporales);
-//         })
-//         .catch(error => console.error('Error al acceder a Google Sheets:', error));
-// });
-
 document.addEventListener("DOMContentLoaded", function () {
     // Función para cargar las promociones en los contenedores HTML
     function cargarPromociones(containerId, promociones) {
